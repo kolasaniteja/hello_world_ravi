e461d70a5c3ef50e45922c4bfa16b0ed71154a1a
ravitejakolasani <ravi.kolasani@octanner.com>
2018-04-04 10:10:53 +0530

    proj

---
+++ src/main/java/test_ci/test_ci/App.java
@@ -0,0 +1,13 @@
+package test_ci.test_ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
